bab51eaadf2542ef8eb20ad6b87a021cc3fbcb21
nivamoli <github@nicovalencia.com.co>
2021-07-19 15:18:09 -0500

    Variables

---
+++ basico_js/variables.js
@@ -0,0 +1,32 @@
+var // palabra reservada para definir variables - reservar espacio en memoria. 
+
+var nombre = 'Eduardo';
+
+// estados de una variable:
+
+// - Declarar: 
+
+var apellido; // estamos reservando un espacio en memoria con el nombre 'apellido' para después pasarle un valor. 
+
+// - inicializar: 
+
+apellido = 'Valencia'; // cuando le definimos el valor a esa variable
+
+
+// definiendo un array 
+
+var elementos = ["computador", "bicicleta", "laptop"]
+
+// definiendo un objeto 
+
+var nombre = {
+    nombre: "norberto",
+    apellido: "sanchez",
+    edad: 25
+};
+
+
+
+
+
+
