c329d3fed6d1905047d920e0fe2c6e1d57d438c9
nivamoli <github@nicovalencia.com.co>
2021-07-21 17:00:42 -0500

    condicional switch

---
+++ condicionales/switch.js
@@ -0,0 +1,22 @@
+// nos permite validar varios casos de condiciones. 
+
+var numero = 1; 
+
+switch (numero) {
+    case 1: 
+        console.log('Soy un 1');
+    break;
+    case 2: 
+        console.log('soy un 2')
+    break; // con break cortamos la ejecución del caso.
+    case 3: 
+        console.log('Soy un 3')
+    break; // // debemos declarar siempre el break para evitar que todos los procesos se ejecuten. 
+    default: 
+        console.log('Soy un 10'); 
+} 
+
+
+
+
+
