b13b1bc5957c17839925b9866bad7b08efd44717
nivamoli <github@nicovalencia.com.co>
2021-07-28 15:34:57 -0500

    convirtiendo constantes en funciones

--- practicoJS/figuras-geometricas/figuras_geometricas.js
+++ practicoJS/figuras-geometricas/figuras_geometricas.js
@@ -5,41 +5,82 @@
 // perímetro del círculo = (Diámetro * PI) Radio -> de la mitad hacia el borde del círculo - Diámetro -> Radio * 2.  
 // Área del círculo = Radio al cuadrado * PI.
 
+
+
+
+
+
+
 // Código para el cuadrado:
 
 console.group("Cuadrados")
 
-const ladoCuadrado = 5; 
-console.log(`Los lados del cuadrado miden: ${ladoCuadrado}cm`)
+// console.log(`Los lados del cuadrado miden: ${ladoCuadrado}cm`)
 
-const perimetroCuadrado = ladoCuadrado * 4; 
-console.log(`El perímetro del cuadrado es: ${perimetroCuadrado}cm`)
+function perimetroCuadrado(lado) {
+    return lado * 4
+}
 
-const areaCuadrado = ladoCuadrado * ladoCuadrado; 
-console.log(`El area del cuadrado es: ${areaCuadrado}cm2`);
+function areaCuadrado(lado) {
+    return lado * lado;
+}
+
+perimetroCuadrado(56);
+// console.log(`El perímetro del cuadrado es: ${perimetroCuadrado}cm`)
+
+areaCuadrado(56);
+
+// console.log(`El area del cuadrado es: ${areaCuadrado}cm2`);
 
 console.groupEnd();
+
+
+
+
+
+
+
+
 
 // Código para el triángulo: 
 
 console.group("Triangulos")
 
-const ladoTriangulo1 = 6;
-const ladoTriangulo2 = 6;
-const baseTriangulo = 4;
-const alturaTriangulo = 5.5; 
-const perimetroTriangulo = ladoTriangulo1 + ladoTriangulo2 + baseTriangulo;
-const areaTriangulo = (baseTriangulo * alturaTriangulo) / 2; // definimos dentro de paréntesis para que ejecute primero esa operación
+// const ladoTriangulo1 = 6;
+// const ladoTriangulo2 = 6;
+// const baseTriangulo = 4;
+// const alturaTriangulo = 5.5; 
+// const perimetroTriangulo = ladoTriangulo1 + ladoTriangulo2 + baseTriangulo;
+// const areaTriangulo = (baseTriangulo * alturaTriangulo) / 2; // definimos dentro de paréntesis para que ejecute primero esa operación
 
-console.log(
-    `El lado 1 mide: ${ladoTriangulo1}cm, 
-    El lado 2 mide: ${ladoTriangulo2}cm 
-    La base mide: ${baseTriangulo}cm
-    La altura es de: ${alturaTriangulo}cm
-    El perímetro del triángulo es: ${perimetroTriangulo}cm
-    El área del triángulo es de: ${areaTriangulo}cm2`)
+function perimetroTriangulo(lado1, lado2, base) {
+    return lado1 + lado2 + base;
+}
+
+function areaTriangulo(base, altura) {
+    return (base * altura) / 2; 
+}
+
+perimetroTriangulo(23, 12, 5);
+areaTriangulo(5, 23);
+
+// console.log(
+//     `El lado 1 mide: ${ladoTriangulo1}cm, 
+//     El lado 2 mide: ${ladoTriangulo2}cm 
+//     La base mide: ${baseTriangulo}cm
+//     La altura es de: ${alturaTriangulo}cm
+//     El perímetro del triángulo es: ${perimetroTriangulo}cm
+//     El área del triángulo es de: ${areaTriangulo}cm2`)
 
 console.groupEnd();
+
+
+
+
+
+
+
+
 
 
 // Código para el círculo:
@@ -47,16 +88,33 @@ console.groupEnd();
 console.group("Círculos")
 
 // const numPi = 3.14; // podemos definirlo manualente ó:
-const pi = Math.PI;
-const radioCirculo = 4;
-const diametroCirculo = radioCirculo * 2;
-const perimetroCirculo = diametroCirculo * pi;
-const areaCirculo = radioCirculo ** 2 * pi;
+// const pi = Math.PI;
+// const radioCirculo = 4;
+// const diametroCirculo = radioCirculo * 2;
+// const perimetroCirculo = diametroCirculo * pi;
+// const areaCirculo = radioCirculo ** 2 * pi;
 
-console.log(
-    `El radio del círculo es de: ${radioCirculo}cm, 
-    El diámetro del circulo es de: ${diametroCirculo}cm 
-    El perímetro del círculo es de: ${perimetroCirculo}
-    El área del circulo es de: ${areaCirculo}cm2`)
+const pi = Math.PI;
+
+function diametroCirculo(radio) {
+    return radio * 2;
+}
+
+function perimetroCirculo(radio) {
+    //diametro = radio * 2; // en lugar de repetir la formula para calcular el radio: llamamos la funcion
+    const diametro = diametroCirculo(radio); 
+    return diametro * pi;
+}
+
+function areaCirculo(radio) {
+    return (radio ** 2) * pi
+}
+
+
+// console.log(
+//     `El radio del círculo es de: ${radioCirculo}cm, 
+//     El diámetro del circulo es de: ${diametroCirculo}cm 
+//     El perímetro del círculo es de: ${perimetroCirculo}
+//     El área del circulo es de: ${areaCirculo}cm2`)
 
 console.groupEnd();
