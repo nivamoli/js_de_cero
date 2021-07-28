560cf2221436246e00f4fc17ed71132324ec17e8
nivamoli <github@nicovalencia.com.co>
2021-07-28 10:40:46 -0500

    archivos iniciales para calcular figuras geometricas

---
+++ README.md
@@ -0,0 +1,14 @@
+# Curso práctico de JS 
+
+... 
+
+## Taller fifuras geométricas: 
+
+- Primer paso: Definir fórmulas.
+
+- Implementar fórmulas en JS 
+
+- Crear funciones 
+
+- Integrar JS con HTML 
+
---
+++ practicoJS/figuras-geometricas/figuras_geometricas.js
@@ -0,0 +1,7 @@
+// perimetro del cuadrado = suma de sus 4 lados
+// Área del cuadrado = multiplicar 2 de sus lados. (un lado al cuadrado)
+// perímetro del triángulo = Suma de sus 3 lados 
+// Área del triángulo = Base * altura / 2
+// perímetro del círculo = (Diámetro * PI) Radio -> de la mitad hacia el borde del círculo - Diámetro -> Radio * 2.  
+// Área del círculo = Radio al cuadrado * PI.
+
