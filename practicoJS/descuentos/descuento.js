d0b19762e096547ec672a374ed408a868debcd2f
nivamoli <github@nicovalencia.com.co>
2021-08-03 16:53:00 -0500

    formulas y estilo de calcular descuento terminado

--- practicoJS/descuentos/descuento.js
+++ practicoJS/descuentos/descuento.js
@@ -2,10 +2,29 @@
 // [precio * (100 - descuento)] / 100
 
 
+// function calcularDescuento(precioOriginal, descuento) {
+//     const porcentajePrecioConDescuento = 100 - descuento; 
+//     const precioConDescuento = (precioOriginal * porcentajePrecioConDescuento) / 100; 
+//     const pgraphResult = document.getElementById("price-graph");
+//     pgraphResult.innerText = (`El precio a pagar con el descuento aplicado es de: $${precioConDescuento} dolares`)
+// }
+
+
+// function devolverPrecioConDescuento () {
+//     const inputPrecio = document.getElementById('precio');
+//     const precio = inputPrecio.value;
+//     const inputDescuento = document.getElementById('descuento')
+//     const descuento = inputDescuento.value;
+//     const valorProducto = calcularDescuento(precio, descuento)
+//     return valorProducto;
+// }
+
+
 function calcularDescuento(precioOriginal, descuento) {
     const porcentajePrecioConDescuento = 100 - descuento; 
     const precioConDescuento = (precioOriginal * porcentajePrecioConDescuento) / 100; 
-    alert(`El precio a pagar con el descuento aplicado es de: ${precioConDescuento} dolares`)
+    const pgraphResult = document.getElementById("price-pgraph");
+    pgraphResult.innerText = (`El precio a pagar con el descuento aplicado es de: $${precioConDescuento} dolares`)
 }
 
 
@@ -14,6 +33,16 @@ function devolverPrecioConDescuento () {
     const precio = inputPrecio.value;
     const inputDescuento = document.getElementById('descuento')
     const descuento = inputDescuento.value;
-    const valorProducto = calcularDescuento(precio, descuento)
-    return valorProducto;
+    if(precio <=0 || descuento <=0) {
+        const pGraphError = document.getElementById("error-pgraph");
+        pGraphError.innerText = "Por favor ingrese valores correctos";
+    } else {
+        const valorProducto = calcularDescuento(precio, descuento)
+        return valorProducto;
+    }
 }
+
+
+
+
+
