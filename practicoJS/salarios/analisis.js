6fb1460de2c39d644a99e3d6ab0ae92fc4dcd3ef
nivamoli <github@nicovalencia.com.co>
2021-08-09 15:15:06 -0500

    top 10% salarios

--- practicoJS/salarios/analisis.js
+++ practicoJS/salarios/analisis.js
@@ -1,13 +1,7 @@
 
-const listaSalarios = colombia.map(function(persona) {
-    return persona.salary;
-});
+// Helpers ó Utils -> Funciones que no son del programa principal pero que sirven para el fin de este. 
 
-const salariosOrdenados = listaSalarios.sort(function(salaryA, salaryB) {
-    return salaryA - salaryB;
-})
-
-
+// calcular mediana general
 function mediaAritmetica(lista) {
     const sumaLista = lista.reduce(function(varlorAcum = 0, elemento) {
         varlorAcum = varlorAcum + elemento; 
@@ -22,6 +16,11 @@ function esPar(numero) {
     return (numero % 2 === 0); 
 }
 
+// Helpers /// 
+
+
+// ----- calcular mediana salarios
+
 function medianaSalarios(lista) {
     const mitad = parseInt(lista.length / 2)
     if(esPar(lista.length)) {
@@ -35,4 +34,33 @@ function medianaSalarios(lista) {
     }
 }
 
-console.log(medianaSalarios(salariosOrdenados));
+const listaSalarios = colombia.map(function(persona) {
+    return persona.salary;
+});
+
+const salariosOrdenados = listaSalarios.sort(function(salaryA, salaryB) {
+    return salaryA - salaryB;
+})
+
+const medianaGeneralCol = medianaSalarios(salariosOrdenados);
+
+
+
+// ----- calcular mediana Top 10%
+
+
+
+const spliceStart = (salariosOrdenados.length * 90) / 100;
+const spliceCount = salariosOrdenados.length - spliceStart;
+
+const salaryTop10 = salariosOrdenados.splice(
+    spliceStart,
+    spliceCount
+)
+
+const medianaTop10Col = medianaSalarios(salaryTop10);
+
+console.log({
+    medianaGeneralCol,
+    medianaTop10Col
+})
